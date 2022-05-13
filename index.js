293eff3012e26e1ae5a9caa0e54140fd58376322
Ebere Okezie <ebereokezie@gmail.com>
2022-05-13 14:42:40 -0500

    commit tree ae5a36796b55c4b33b6d0452019d4b06073e323d

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+// function hasTargetSum(numbers, targetSum) {
+//   let newCollection=[]
+//   for(let i =0; i< numbers.length; i++){
+//       // console.log(numbers[i])
+//     for(let j =i+1; j<numbers.length; j++){
+//   newCollection.push(numbers[i]+numbers[j])
+//       }
+//   }
+//   let newTargetSum = newCollection.find(e => e === targetSum)
+//   if(newTargetSum === targetSum){
+//     return true
+//   } else {
+//     return false
+//   }
+  
+// }
+
+
+function hasTargetSum(array, target){
+  for(let i = 0; i<array.length; i++){
+    for(let j= i+1; j<array.length; j++){
+      if(array[i]+array[j] === target) return true
+    }
+  }
+return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
-  Add your pseudocode here
+  Create a new, blank array
+  Iterate over the numbers to create all possible solutions
+  Push the solutions to the new array
+  Look through new array to see if any solutions equal target
+  if so, return true; else, return false
 */
 
 /*
-  Add written explanation of your solution here
+  I want to add all the numbers together in the array and come up with all possible solutions.
+  I want to move these solutions to another array, and then see if any of these solutions match my target
+  If they do, return it true. If they don't return it false.
 */
 
 // You can run `node index.js` to view these console logs
